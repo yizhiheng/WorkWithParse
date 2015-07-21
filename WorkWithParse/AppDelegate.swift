17eeea48683956040204dae5cb082ace1f9faace
LEO <120039015@qq.com>
2015-07-21 16:37:49 -0400

    connected to Parse

--- WorkWithParse/AppDelegate.swift
+++ WorkWithParse/AppDelegate.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import CoreData
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        Parse.setApplicationId("RSGEem6LiULIkDN0NEGEbq7HwLP9FqYKoRgnzoKW", clientKey: "L6WQlm13GhpoxvgbFfx2RGuqzVaPGR8G9IDCQqko")
         return true
     }
 
--- WorkWithParse/ViewController.swift
+++ WorkWithParse/ViewController.swift
@@ -9,17 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let query = PFQuery(className:"DemoData")
+        query.findObjectsInBackgroundWithBlock {
+            (objects: [AnyObject]?, error: NSError?) -> Void in
+            if error == nil {
+                // Successfully retrieved the objects from Parse
+                if let objects = objects as? [PFObject] {
+                    println(objects[0])
+                // Do something
+                }
+            } else {
+                // Log details of the failure
+                println("Error: \(error) \(error!.userInfo)")
+            }
+        }
+        
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+                super.didReceiveMemoryWarning()
+                // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
