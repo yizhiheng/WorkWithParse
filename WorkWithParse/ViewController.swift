d5d988f6416479c1fa38b23af673de270dc65035
LEO <120039015@qq.com>
2015-07-21 16:07:02 -0400

    Initial Commit

---
+++ WorkWithParse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkWithParse
+//
+//  Created by Zhiheng Yi on 2015-07-21.
+//  Copyright (c) 2015 Zhiheng Yi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
